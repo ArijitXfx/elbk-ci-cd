1b96c8e7fb051a3f2a9f4e49071735407cbdacd7
ArijitXfx <thearijitxfx@gmail.com>
2021-05-16 17:30:11 +0530

    first commit

---
+++ src/main/java/com/arijit/elbkcicd/ElbkCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.arijit.elbkcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElbkCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElbkCiCdApplication.class, args);
+	}
+
+}
